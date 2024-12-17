c2c48cbca337f11a6f13d354932be119f6c85dfb
rptl8sr <rptl8sr@gmail.com>
2024-12-18 00:17:06 +0300

    Add init config and logger to app

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,5 +1,10 @@
 package app
 
+import (
+	"calculate-service/internal/config"
+	"calculate-service/internal/logger"
+)
+
 type app struct {
 }
 
@@ -8,6 +13,18 @@ type App interface {
 }
 
 func MustLoad() (App, error) {
+	cfg, err := config.MustLoad()
+	if err != nil {
+		return nil, err
+	}
+
+	logger.Init(cfg.App.LogLevel)
+
+	if cfg.App.Mode == config.Development {
+		logger.Debug("Dev mode", "config", cfg)
+	}
+
+	logger.Info("App initialized", "mode", cfg.App.Mode, "logLevel", cfg.App.LogLevel)
 	return &app{}, nil
 }
 
