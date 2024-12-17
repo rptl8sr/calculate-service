75a71d8edd3e51dbbea3822176a00d8f9a3784c9
rptl8sr <rptl8sr@gmail.com>
2024-12-18 01:11:53 +0300

    Update router

--- internal/router/router.go
+++ internal/router/router.go
@@ -1,6 +1,7 @@
 package router
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/go-chi/chi/v5"
@@ -14,15 +15,16 @@ func New(ctrl controller.Controller, apiVersion string) *chi.Mux {
 	h := handlers.New(ctrl)
 	r := chi.NewRouter()
 
-	r.Use(middleware.RequestID)
-	r.Use(middleware.Timeout(60 * time.Second))
-	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
-	//r.Use(middleware.URLFormat)
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Timeout(30 * time.Second))
+
 	r.Use(middleware.Heartbeat("/ping"))
 
 	r.Route("/api", func(r chi.Router) {
-		r.Route(apiVersion, func(r chi.Router) {
+		r.Route(fmt.Sprintf("/%s", apiVersion), func(r chi.Router) {
 			r.Post("/calculate", h.Calculate)
 		})
 	})
